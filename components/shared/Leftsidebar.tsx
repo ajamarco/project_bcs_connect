82de8a48c2868799f0217795613705166b9d4be9
Alex Jamarco <a.jamarco@gmail.com>
2024-02-03 15:13:16 +0000

    coded left side bar

--- components/shared/Leftsidebar.tsx
+++ components/shared/Leftsidebar.tsx
@@ -1,5 +1,64 @@
+"use client";
+
+//import libraries
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation"; //this will allow us to use the router and pathname, so we can highlight the active link
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+
+//import link names
+import { sidebarLinks } from "@constants/index";
+
 const Leftsidebar = () => {
-  return <div>Leftsidebar</div>;
+  const pathname = usePathname();
+  const router = useRouter();
+
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          //check if the link is active
+          const isActive =
+            (pathname.includes(link.route) && link.route !== "/") ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton
+            signOutCallback={() => {
+              router.push("/sign-in");
+            }}
+          >
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={20}
+                height={20}
+              />
+              <p className="text-light-1 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
 };
 
 export default Leftsidebar;
