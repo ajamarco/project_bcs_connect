946f784eb45ceede71478def0a9433bfaec7a587
Alex Jamarco <a.jamarco@gmail.com>
2024-02-07 17:41:20 +0000

    installed shadcn, zod, and started setting up the onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,5 +1,32 @@
-const page = () => {
-  return <div>Onboarding</div>;
+//import libraries
+import { currentUser } from "@clerk/nextjs"; //get the current logged user
+
+//import components
+import AccountProfile from "@/components/forms/AccountProfile";
+
+const page = async () => {
+  const user = await currentUser(); //get the current logged user
+
+  const userInfo = {}; //TODO: get the user info from the database
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user.imageUrl,
+  };
+  return (
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now
+      </p>
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
+    </main>
+  );
 };
 
 export default page;
